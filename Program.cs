6c797d42cd1da2dc9c606faf99690b6d5673d116
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-19 13:44:01 -0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+double numero, avaliar, resultado;
+
+Console.WriteLine("--- multiplo ---\n");
+
+Console.Write("Digite um numero..........: ");
+numero = Int32.Parse(Console.ReadLine()!);
+
+Console.Write("Avaliar se e multiplo por.: ");
+avaliar = Int32.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+resultado = numero % avaliar;
+
+if (resultado != 0)
+{
+    Console.WriteLine($"{numero} não é multiplo de {avaliar}");
+}
+else
+{
+    Console.WriteLine($"{numero} é multiplo de {avaliar}");
+}
